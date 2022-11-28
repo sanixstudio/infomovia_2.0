83c5f75ae1ce5a032610c1aafec562f4947082d9
Adnan Niaz <sanixstudio@gmail.com>
2022-11-28 14:09:48 -0800

    chore: added poster backdrop and other

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -4,9 +4,15 @@ import './poster.styles.css'
 
 const Poster = ({ movieInfo }) => {
     const POSTER_IMG = IMG_URL + movieInfo.poster_path
+    const YEAR_RELEASED = movieInfo.release_date.split("-")[0]
+
     return (
         <div className='poster'>
             <img src={POSTER_IMG} alt="" />
+            <p className='title'>{movieInfo.title}</p>
+            <div className='poster-backdrop'>
+                <p>{movieInfo.title} ({YEAR_RELEASED})</p>
+            </div>
         </div>
     )
 }
