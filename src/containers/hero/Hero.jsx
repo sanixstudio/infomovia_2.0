af260287ceafc19c12258d1952ec42d2678fd7e1
Adnan Niaz <sanixstudio@gmail.com>
2022-11-28 04:17:10 -0800

    chore: added carousel package

--- src/containers/hero/Hero.jsx
+++ src/containers/hero/Hero.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Hero = () => {
   return (
-    <div style={{minHeight: '50vh'}}>
-      <h1>Hero</h1>
+    <div style={{ minHeight: '50vh' }}>
+      <Carousel>
+        <div>
+          <img src="https://picsum.photos/id/237/700/300" alt="" />
+          <p></p>
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/237/700/300" alt="" />
+          <p></p>
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/237/700/300" alt="" />
+          <p></p>
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/237/700/300" alt="" />
+          <p></p>
+        </div>
+        <div>
+          <img src="https://picsum.photos/id/237/700/300" alt="" />
+          <p></p>
+        </div>
+      </Carousel>
     </div>
   );
 }
