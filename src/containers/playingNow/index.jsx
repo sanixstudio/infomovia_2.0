f71706002f0e4682053ab21abda0e7be80f24a61
Adnan Niaz <sanixstudio@gmail.com>
2022-11-28 00:53:28 -0800

    fix: fixed env variable issue

--- src/containers/playingNow/index.jsx
+++ src/containers/playingNow/index.jsx
@@ -4,10 +4,9 @@ import { fetchMoviesList } from "../../hooks";
 import { ThreeDots } from "react-loader-spinner";
 
 const PlayingNow = () => {
-  //TODO: Fix ENV issue for API keys
-  const API_KEY = "1dc3a1bc9c9d1a12ed9931344d82ebc1";
-  const END_POINT = `${REACT_APP_BASE_URL}movie/upcoming?language=en-US&api_key=${API_KEY}`;
-  const { status, data, error } = useQuery(["moviesList"], () =>
+  const VITE_API_KEY = import.meta.env.VITE_API_KEY
+  const END_POINT = `${REACT_APP_BASE_URL}movie/upcoming?language=en-US&api_key=${VITE_API_KEY}`;
+  const { status } = useQuery(["moviesList"], () =>
     fetchMoviesList(END_POINT)
   );
 
