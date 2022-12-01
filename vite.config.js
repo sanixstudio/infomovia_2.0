c176622c09fd0ac357a4d8d78d26a842dc9cc9fa
Adnan Niaz <sanixstudio@gmail.com>
2022-12-01 03:54:55 -0800

    fixing issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': {}
+    "process.env": {},
   },
-  plugins: [react()]
-})
+  root: path.resolve(__dirname, "src"),
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html"),
+        about: path.resolve(__dirname, "src/about.html"),
+      },
+    },
+  },
+});
