b32f18f1ba496361ed9c2c090287f45516ca2090
Adnan Niaz <sanixstudio@gmail.com>
2023-04-20 02:53:07 -0700

    chore: applies styling to genres

--- src/containers/hero/Hero.jsx
+++ src/containers/hero/Hero.jsx
@@ -3,8 +3,8 @@ import { useQuery } from "@tanstack/react-query";
 import { ThreeDots } from "react-loader-spinner";
 import { Carousel } from "react-responsive-carousel";
 import { fetchMoviesList } from "../../services";
-import { BACKDROP_1280, UP_COMING } from "../../utils/constants";
-import {AiFillStar} from 'react-icons/ai'
+import { BACKDROP_1280, GENRES, UP_COMING } from "../../utils/constants";
+import { AiFillStar } from "react-icons/ai";
 
 import "./hero.styles.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -14,7 +14,24 @@ const Hero = () => {
     fetchMoviesList(UP_COMING)
   );
 
-  if (status === "loading")
+  const findMatchingGenres = (ids) => {
+    const genres = GENRES.movies; // you can also change this to GENRES.tvShows if needed
+    return Object.keys(genres)
+      .filter((genre) => ids.includes(genres[genre]))
+      .map((genre) => genre);
+  };
+
+  const getFormattedGenres = (ids) => (
+    <>
+      {findMatchingGenres(ids).map((g) => (
+        <p className="genre" key={g}>
+          {g}
+        </p>
+      ))}
+    </>
+  );
+
+  if (status === "loading") {
     return (
       <ThreeDots
         height="80"
@@ -24,28 +41,44 @@ const Hero = () => {
         ariaLabel="three-dots-loading"
       />
     );
-
-    console.log(data)
+  }
 
   return (
     <div className="hero">
       <Carousel autoPlay infiniteLoop showThumbs={false}>
-        {data.map(({ id, poster_path, title, overview, release_date, vote_average }) => (
-          <div
-            key={id}
-            className="hero-slider"
-            style={{
-              background: `url(${BACKDROP_1280 + poster_path}`,
-            }}
-          >
-            <img src={BACKDROP_1280 + poster_path} alt={title} />
-            <div className="legend">
-              <p className="legend-title">{title} ({release_date.split("-")[0]})</p>
-              <p><AiFillStar color="yellow" /> {vote_average}</p>
-              <p className="legend-overview">{overview}</p>
+        {data.map(
+          ({
+            id,
+            poster_path,
+            title,
+            overview,
+            release_date,
+            vote_average,
+            genre_ids,
+          }) => (
+            <div
+              key={id}
+              className="hero-slider"
+              style={{
+                background: `url(${BACKDROP_1280 + poster_path}`,
+              }}
+            >
+              <img src={BACKDROP_1280 + poster_path} alt={title} />
+              <div className="legend">
+                <p className="legend-title">
+                  {title} ({release_date.split("-")[0]})
+                </p>
+                <div className="legend-info">
+                  <span>
+                    <AiFillStar color="yellow" /> {vote_average}{" "}
+                  </span>
+                  <span>{getFormattedGenres(genre_ids)}</span>
+                </div>
+                <p className="legend-overview">{overview}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </Carousel>
     </div>
   );
--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -6,3 +6,45 @@ export const PLAYING_NOW = `${BASE_URL}movie/now_playing?language=en-US&api_key=
 export const UP_COMING = `${BASE_URL}movie/upcoming?language=en-US&api_key=${API_KEY}`;
 export const TOP_RATED = `${BASE_URL}movie/top_rated?language=en-US&api_key=${API_KEY}`;
 export const SEARCH_URL = `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
+
+export const GENRES = {
+  movies: {
+    action: 28,
+    adventure: 12,
+    animation: 16,
+    comedy: 35,
+    crime: 80,
+    documentary: 99,
+    drama: 18,
+    family: 10751,
+    fantasy: 14,
+    history: 36,
+    horror: 27,
+    music: 10402,
+    mystery: 9648,
+    romance: 10749,
+    scienceFiction: 878,
+    tVMovie: 10770,
+    thriller: 53,
+    war: 10752,
+    western: 37,
+  },
+  tvShows: {
+    actionAndAdventure: 10759,
+    animation: 16,
+    comedy: 35,
+    crime: 80,
+    documentary: 99,
+    drama: 18,
+    family: 10751,
+    kids: 10762,
+    mystery: 9648,
+    news: 10763,
+    reality: 10764,
+    sciFiAndFantasy: 10765,
+    soap: 10766,
+    talk: 10767,
+    warAndPolitics: 10768,
+    western: 37,
+  },
+};
