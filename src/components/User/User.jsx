31dc7a577ad00d5d656f5e8ce2b363cd2d7bfa4f
Adnan Niaz <sanixstudio@gmail.com>
2023-04-20 06:28:21 -0700

    chor: create user-menu dropdown

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,17 +1,39 @@
 import React, { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
+import { IoMdHeart } from "react-icons/io";
+import { MdLogout } from "react-icons/md";
 
 import "./user.styles.css";
 
 const User = () => {
   const [user, setUser] = useState("");
+  const [isOpen, setIsopen] = useState(false);
 
   return (
-    <div>
+    <>
       {user ? (
-        <div>
-          <span>Hi, {user}</span>
-          <a href="#">Logout</a>
+        <div className="user-controls">
+          <div className="user-menu" onClick={() => setIsopen(!isOpen)}>
+            <FaUserCircle size={24} />
+            <span>{user}</span>
+          </div>
+          <div
+            className="user-options"
+            style={{ display: isOpen ? "flex" : "none" }}
+          >
+            <div className="user-profile">
+              <FaUserCircle size={24} />
+              <span>Profile</span>
+            </div>
+            <div className="user-wishlist">
+              <IoMdHeart size={24} />
+              <span>My Wish List</span>
+            </div>
+            <div className="user-logout">
+              <MdLogout size={24} />
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="user-button" onClick={() => alert("hi")}>
@@ -19,7 +41,7 @@ const User = () => {
           <span>Login/ Register</span>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
