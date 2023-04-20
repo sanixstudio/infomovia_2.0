293b818783c60f836d66c53dfd879a365495ed0e
Adnan Niaz <sanixstudio@gmail.com>
2023-04-19 21:51:24 -0700

    fix: fixed hero styling

--- src/containers/hero/Hero.jsx
+++ src/containers/hero/Hero.jsx
@@ -4,6 +4,7 @@ import { ThreeDots } from "react-loader-spinner";
 import { Carousel } from "react-responsive-carousel";
 import { fetchMoviesList } from "../../services";
 import { BACKDROP_1280, UP_COMING } from "../../utils/constants";
+import {AiFillStar} from 'react-icons/ai'
 
 import "./hero.styles.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -24,10 +25,12 @@ const Hero = () => {
       />
     );
 
+    console.log(data)
+
   return (
     <div className="hero">
-      <Carousel autoPlay infiniteLoop>
-        {data.map(({ id, poster_path, title, overview }) => (
+      <Carousel autoPlay infiniteLoop showThumbs={false}>
+        {data.map(({ id, poster_path, title, overview, release_date, vote_average }) => (
           <div
             key={id}
             className="hero-slider"
@@ -36,8 +39,10 @@ const Hero = () => {
             }}
           >
             <img src={BACKDROP_1280 + poster_path} alt={title} />
-            <div>
-              <p className="legend">{overview}</p>
+            <div className="legend">
+              <p className="legend-title">{title} ({release_date.split("-")[0]})</p>
+              <p><AiFillStar color="yellow" /> {vote_average}</p>
+              <p className="legend-overview">{overview}</p>
             </div>
           </div>
         ))}
